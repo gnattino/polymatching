0fa96d366bd5ec2f43c91f3ad6409fbde5d99128
gnattino <35614948+gnattino@users.noreply.github.com>
2021-12-17 17:48:23 +0100

    modified with weights

--- R/balance_functions.R
+++ R/balance_functions.R
@@ -30,25 +30,46 @@ typeVariable <- function(variable) {
   return(type)
 }
 
+#' Function to compute weighted variance
+#' @keywords internal
+weighted.variance <- function(x, w) {
+
+  sum_w <- sum(w)
+  sum_wsq <- sum(w^2)
+  mean <- stats::weighted.mean(x = x, w = w)
+
+  sum(w * (x - mean)^2) * (sum_w / (sum_w^2 - sum_wsq))
+
+}
+
+
 #' Balance indicators for continuous variables
 #' @keywords internal
-balanceContVar <- function(data, varBalance, match_id, varGroup, pairGroups){
+balanceContVar <- function(data, varBalance, match_id, varGroup, pairGroups, varWeights){
+
+  varGroup_symbol <- as.symbol(varGroup)
+  varBalance_symbol <- as.symbol(varBalance)
+  varWeights_symbol <- as.symbol(varWeights)
 
   #For difference in means, matched data
-  dataMeans <- data[data[,varGroup] %in% pairGroups & !is.na(match_id), ]
-  means <- tapply(dataMeans[,varBalance], INDEX = dataMeans[,varGroup], FUN = mean)
-  vars <- tapply(dataMeans[,varBalance], INDEX = dataMeans[,varGroup], FUN = stats::var)
+  dataMatch <- data[data[,varGroup] %in% pairGroups & !is.na(match_id), ]
+  infoMatch <- dataMatch %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(means = stats::weighted.mean(x = !!varBalance_symbol, w = !!varWeights_symbol),
+                     vars = weighted.variance(x = !!varBalance_symbol, w = !!varWeights_symbol))
 
   #For variances, unmatched data
   dataUnm <- data[data[,varGroup] %in% pairGroups, ]
-  varsUnm <- tapply(dataUnm[,varBalance], INDEX = dataUnm[,varGroup], FUN = stats::var)
+  infoUnm <- dataUnm %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(vars = weighted.variance(x = !!varBalance_symbol, w = !!varWeights_symbol))
 
-  stdzDiff <- (means[names(means)==pairGroups[1]] - means[names(means)==pairGroups[2]])/(
-                sqrt((varsUnm[names(varsUnm)==pairGroups[1]] + varsUnm[names(varsUnm)==pairGroups[2]])/2))
+  stdzDiff <- (infoMatch$means[infoMatch[,varGroup]==pairGroups[1]] - infoMatch$means[infoMatch[,varGroup]==pairGroups[2]])/(
+                sqrt((infoUnm$vars[infoUnm[,varGroup]==pairGroups[1]] + infoUnm$vars[infoUnm[,varGroup]==pairGroups[2]])/2))
 
   #Return the ratio of the variances >= 1
-  ratioVars <- max(c(vars[names(vars)==pairGroups[1]]/vars[names(vars)==pairGroups[2]],
-                     vars[names(vars)==pairGroups[2]]/vars[names(vars)==pairGroups[1]]))
+  ratioVars <- max(c(infoMatch$vars[infoMatch[,varGroup]==pairGroups[1]]/infoMatch$vars[infoMatch[,varGroup]==pairGroups[2]],
+                     infoMatch$vars[infoMatch[,varGroup]==pairGroups[2]]/infoMatch$vars[infoMatch[,varGroup]==pairGroups[1]]))
 
   return(list(stdzDiff = stdzDiff,
               ratioVars = ratioVars))
@@ -57,22 +78,30 @@ balanceContVar <- function(data, varBalance, match_id, varGroup, pairGroups){
 
 #' Balance indicators for binary variables
 #' @keywords internal
-balanceBinVar <- function(data, varBalance, match_id, varGroup, pairGroups){
+balanceBinVar <- function(data, varBalance, match_id, varGroup, pairGroups, varWeights){
+
+
+  varGroup_symbol <- as.symbol(varGroup)
+  varWeights_symbol <- as.symbol(varWeights)
 
   #The "1" in the binary variables is considered the second level
   data$varBalanceNum <- (data[,varBalance] == (levels(data[,varBalance])[2]))*1
 
   #For difference in means, matched data
-  dataMeans <- data[data[,varGroup] %in% pairGroups &  !is.na(match_id), ]
-  means <- tapply(dataMeans$varBalanceNum, INDEX = dataMeans[,varGroup], FUN = mean)
+  dataMatch <- data[data[,varGroup] %in% pairGroups & !is.na(match_id), ]
+  infoMatch <- dataMatch %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(means = stats::weighted.mean(x = varBalanceNum, w = !!varWeights_symbol))
 
   #For variances, unmatched data
-  dataMeansUnm <- data[data[,varGroup] %in% pairGroups, ]
-  meansUnm <- tapply(dataMeansUnm$varBalanceNum, INDEX = dataMeansUnm[,varGroup], FUN = mean)
-  vars <- meansUnm*(1-meansUnm)
+  dataUnm <- data[data[,varGroup] %in% pairGroups, ]
+  infoUnm <- dataUnm %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(means = stats::weighted.mean(x = varBalanceNum, w = !!varWeights_symbol)) %>%
+    dplyr::mutate(vars = means * (1 - means))
 
-  stdzDiff <- (means[names(means)==pairGroups[1]] - means[names(means)==pairGroups[2]])/(
-               sqrt((vars[names(vars)==pairGroups[1]] + vars[names(vars)==pairGroups[2]])/2))
+  stdzDiff <- (infoMatch$means[infoMatch[,varGroup]==pairGroups[1]] - infoMatch$means[infoMatch[,varGroup]==pairGroups[2]])/(
+               sqrt((infoUnm$vars[infoUnm[,varGroup]==pairGroups[1]] + infoUnm$vars[infoUnm[,varGroup]==pairGroups[2]])/2))
 
   return(list(stdzDiff=stdzDiff))
 
@@ -80,24 +109,50 @@ balanceBinVar <- function(data, varBalance, match_id, varGroup, pairGroups){
 
 #' Balance indicators for categorical variables (more than 2 levels)
 #' @keywords internal
-balanceCatVar <- function(data, varBalance, match_id, varGroup, pairGroups){
+balanceCatVar <- function(data, varBalance, match_id, varGroup, pairGroups, varWeights){
+
+  varGroup_symbol <- as.symbol(varGroup)
+  varBalance_symbol <- as.symbol(varBalance)
+  varWeights_symbol <- as.symbol(varWeights)
 
   #For difference in means, matched data
-  dataMeans <- data[data[,varGroup] %in% pairGroups & !is.na(match_id), ]
+  dataMatch <- data[data[,varGroup] %in% pairGroups & !is.na(match_id),]
+  #Generate dummy variables
+  dummyMatch <- as.data.frame(stats::model.matrix(stats::as.formula(paste("~ -1 + ", varBalance)),
+                                           data = dataMatch))
+  dataMatch <- cbind(dataMatch, dummyMatch)
+  infoMatch <- dataMatch %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(dplyr::across(names(dummyMatch),
+                                   ~stats::weighted.mean(x = .x, w = !!varWeights_symbol)),
+                     .groups = "drop")
 
-  p1 <- prop.table(table(dataMeans[dataMeans[,varGroup] %in% pairGroups[1],varBalance]))
-  p2 <- prop.table(table(dataMeans[dataMeans[,varGroup] %in% pairGroups[2],varBalance]))
+
+  p1 <- as.matrix(infoMatch[infoMatch[,varGroup] == pairGroups[1], names(dummyMatch)])
+  p2 <- as.matrix(infoMatch[infoMatch[,varGroup] == pairGroups[2], names(dummyMatch)])
 
   p1 <- p1[1:(length(p1)-1)]
   p2 <- p2[1:(length(p2)-1)]
 
   #For variances, unmatched data
-  dataMeansUnm <- data[data[,varGroup] %in% pairGroups, ]
-  p1unm <- prop.table(table(dataMeansUnm[dataMeansUnm[,varGroup] %in% pairGroups[1],varBalance]))
-  p2unm <- prop.table(table(dataMeansUnm[dataMeansUnm[,varGroup] %in% pairGroups[2],varBalance]))
+  dataUnm <- data[data[,varGroup] %in% pairGroups,]
+  #Generate dummy variables
+  dummyUnm <- as.data.frame(stats::model.matrix(stats::as.formula(paste("~ -1 + ", varBalance)),
+                                           data = dataUnm))
+  dataUnm <- cbind(dataUnm, dummyUnm)
+  infoUnm <- dataUnm %>%
+    dplyr::group_by(!!varGroup_symbol) %>%
+    dplyr::summarize(dplyr::across(names(dummyUnm),
+                                   ~stats::weighted.mean(x = .x, w = !!varWeights_symbol)),
+                     .groups = "drop")
+
+
+  p1unm <- as.matrix(infoUnm[infoUnm[,varGroup] == pairGroups[1], names(dummyUnm)])
+  p2unm <- as.matrix(infoUnm[infoUnm[,varGroup] == pairGroups[2], names(dummyUnm)])
 
   p1unm <- p1unm[1:(length(p1unm)-1)]
   p2unm <- p2unm[1:(length(p2unm)-1)]
+
 
   Stemp <- (as.matrix(p1unm) %*% t(as.matrix(p1unm)) + as.matrix(p1unm) %*% t(as.matrix(p1unm)))/2
   diag(Stemp) <- 0
